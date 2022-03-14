8e63287c4d6052338c5857ba2bfb0399f6696591
Carlos Diamantino <78228463+CarlosDevlpr@users.noreply.github.com>
2022-03-14 16:59:08 -0300

    Login and Register Pages

--- source/routes.py
+++ source/routes.py
@@ -1,12 +1,21 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
 from source import app, cryptoSearch, request
 
 
 @app.route('/', methods=['GET', 'POST'])
-def form_example():
+def home():
     if request.method == 'POST':
         crypto = str(request.form.get('crypto')).lower()
         currency = str(request.form.get('fiats'))
         cryptodict = cryptoSearch(crypto, currency)
         return render_template('searchresult.html', cryptodict = cryptodict)
     return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
